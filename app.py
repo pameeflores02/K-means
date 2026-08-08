8c22fc0619e61557a1cee9457e064bb55e391124
Pame Aguilar <claudiaaguilar0213@gmail.com>
2026-08-07 23:18:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ def cargar_base():
 modelo, metadata = cargar_modelo()
 df = cargar_base()
 
-mapa = {int(k): v for k, v in metadata['mapa_riesgo'].items()}
+mapa = {int(k): v for k, v in metadata['kmeans']['mapa_riesgo'].items()}
 
-st.caption(metadata['nombre_modelo'])
+st.caption(metadata['proyecto'])
 
 with st.form('datos'):
 
